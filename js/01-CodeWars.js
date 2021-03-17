4182093aaddacc48b66628fad7e018f2f4cc07f1
Tomas <tomas.v.mob@gmail.com>
2021-03-17 18:04:29 +0200

    CW #1

---
+++ js/01-CodeWars.js
@@ -0,0 +1,13 @@
+function positiveSum(arr) {
+    let ps = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 0) {
+            ps += arr[i]
+        }
+    }
+    console.log(ps);
+    return ps;
+}
+
+console.log(positiveSum([1, 2, 4, 4, 5, -6]));
+console.log('Labas rytas');
