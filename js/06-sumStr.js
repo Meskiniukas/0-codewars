821fc0988b84e556682d03a632fc28af52cd00be
Tomas <tomas.v.mob@gmail.com>
2021-03-18 11:34:12 +0200

    CW #6

--- js/06-sumStr.js
+++ js/06-sumStr.js
@@ -1,12 +1,21 @@
 // https://www.codewars.com/kata/5966e33c4e686b508700002d
 
 
+// function sumStr(a, b) {
+//     const pirmas = a === '' ? 0 : parseInt(a);
+//     const antras = b === '' ? 0 : parseInt(b);
+
+//     return '' + (pirmas + antras);
+// }
+
 function sumStr(a, b) {
-    const pirmas = a === '' ? 0 : parseInt(a);
-    const antras = b === '' ? 0 : parseInt(b);
+    const pirmas = Number(a);
+    const antras = Number(b);
 
     return '' + (pirmas + antras);
 }
+
+
 
 console.log(sumStr("4", "5"), '->', "9");
 console.log(sumStr("34", "5"), '->', "39");
