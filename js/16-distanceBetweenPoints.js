a2dbc90b17b239fd713302b1862b1e23e538938a
Tomas <tomas.v.mob@gmail.com>
2021-03-22 14:56:44 +0200

    CW #16

---
+++ js/16-distanceBetweenPoints.js
@@ -0,0 +1,8 @@
+function distanceBetweenPoints(a, b) {
+    const deltaX = a[0] - b[0];
+    const deltaY = a[1] - b[1];
+    const distance = Math.sqrt (deltaX * deltaX + deltaY * deltaY);
+    return distance.toFixed(6);
+  }
+
+  console.log(distanceBetweenPoints([-10.2, 12.5], [0.3, 14.7]));
