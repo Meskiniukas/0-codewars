59db435c4b7217a9c146c66aae873ec601d09f7c
Tomas <tomas.v.mob@gmail.com>
2021-03-27 18:31:37 +0200

    CW #23

---
+++ js/23-removeExclamationMarks.js
@@ -0,0 +1,11 @@
+function removeExclamationMarks(s) {
+    let newString = '';
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] !== '!') {
+        newString += s[i];
+      }
+    }
+    return newString;
+  }
+
+console.log(removeExclamationMarks('Labas ! Rytas ! Lietuva !'));
